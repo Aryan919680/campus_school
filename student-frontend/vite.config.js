cb09995afd0c4c0f21f1191e12cf9857419cf700
Aryan Malpotra <aryanmalpotra@gmail.com>
2025-03-24 17:09:45 +0530

    changes

--- student-frontend/vite.config.js
+++ student-frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/student',
+  // base: '/student',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
