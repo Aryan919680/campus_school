2b4a0952999491ff670ee2e478f947e1a099b1aa
Aryan Malpotra <aryan.malpotra@originscale.io>
2025-03-24 16:51:27 +0530

    vhsngrs

--- employee-frontend/vite.config.js
+++ employee-frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/employee-frontend/',
+  base: '/employee',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
--- student-frontend/vite.config.js
+++ student-frontend/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/student-frontend/',
+  base: '/student',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
